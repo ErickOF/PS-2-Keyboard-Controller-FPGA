03676f6ea59dcfc4d757880d166cdad234c8c917
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2019-11-19 16:01:59 -0600

    Keyboard works

--- KeyboardDisplay.sv
+++ KeyboardDisplay.sv
@@ -1,27 +1,13 @@
 module KeyboardDisplay(input  logic clk,
                        input  logic ps2_clk,
                        input  logic data,
-                        output logic [4:0] leds);
+                       output logic [6:0] deco1, deco2);
 
 logic [7:0] keycode = 0;
 
 KeyboardController KC(.ps2_clk(ps2_clk), .data(data), .code(keycode));
 
-always @(keycode) begin
-    case(keycode)
-        // Up
-        8'h75:   leds <= 5'b00001;
-	     // Down
-	     8'h72:   leds <= 5'b00010;
-	     // Left
-	     8'h6B:   leds <= 5'b00100;
-	     // Right
-	     8'h74:   leds <= 5'b01000;
-		  // Space
-		  8'h29:   leds <= 5'b01000;
-	     // Other key
-	     default: leds <= 5'b10000;
-    endcase
-end
+Decoder D1(keycode[3], keycode[2], keycode[1], keycode[0], deco1);
+Decoder D2(0, keycode[6], keycode[5], keycode[4], deco2);
 
 endmodule // KeyboardDisplay
---
+++ output_files/PS2-Keyboard-Controller.cdf
@@ -0,0 +1,15 @@
+/* Quartus Prime Version 18.1.0 Build 625 09/12/2018 SJ Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Ign)
+		Device PartName(SOCVHPS) MfrSpec(OpMask(0));
+	P ActionCode(Cfg)
+		Device PartName(5CSEMA5F31) Path("C:/Users/ErickOF/Documents/Git/PS-2-Keyboard-Controller/output_files/") File("PS2-Keyboard-Controller.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
